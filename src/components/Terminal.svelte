d3099c63c5bbac6e38b3d6de84663292f939432e
GeorgyPk <georgy.pokazeev@tak.fi>
2024-06-28 20:27:40 +0300

    Add static adapter, add more content, automatic help command content generation

--- src/components/Terminal.svelte
+++ src/components/Terminal.svelte
@@ -7,18 +7,22 @@
     let output = "";
     let outputElement: any;
     
-    const commands: {[key: string]: string} = {
-        "help": "Supported commands: \nhelp, aboutme, skills, experience, education, contact, github, whotohire",
-        "aboutme": "I am passionate software developer and always eager to learn new technologies.",
-        "skills": "Languages: Python, Svelte, JavaScript, TypeScript, CSS\nFrameworks: Django, React\nOther: Databricks, Git",
+    let commands: {[key: string]: string} = {
+        "help": "Supported commands: \n",
+        "aboutme": `As a software developer passionate about creating innovative solutions, I have over two years of experience as a Fullstack Developer with Python (Django), Svelte, JavaScript, SQL (data processing), and PostgreSQL.\nKnown for being a team player, I am always ready to help my colleagues. I am looking for new opportunities to grow as a professional and to contribute to the success of the company.\nI am confident that my reliability, responsibility, and eagerness to learn and tackle new challenges make me the right person to hire.
+        `,
+        "skills": "Languages: Python, Svelte, SQL, JavaScript, TypeScript, CSS\nFrameworks: Django, React\nOther: Databricks, Git",
         "experience": `1. Visitory Oy - Full Stack Software Developer (May 2022 - Present)\n2. Bytetyde - Full Stack Software Developer (Mar 2024 - Present)\n3. LUT University - Study Assistant (Sep 2021 - Jan 2022)`,
         "education": "M.Sc. (Software Engineering) - LUT University (2023 - 2025)\nB.Sc. (Software Engineering) - LUT University (2020 - 2023)",
         "contact": 'Phone: +358456043575\nEmail: <a style="color: #FFFFFF;" href="mailto:geo1529@yahoo.com" target="_blank">geo1529@yahoo.com</a>\nLinkedIn: <a style="color: #FFFFFF;" href="https://www.linkedin.com/in/georgy-pokazeev-6a1381199/" target="_blank">https://www.linkedin.com/in/georgy-pokazeev-6a1381199/</a>',
         "github": 'First account: <a style="color: #FFFFFF;" href="https://github.com/GeorgyPk" target="_blank">https://github.com/GeorgyPk</a>\nSecond account: <a style="color: #FFFFFF;" href="https://github.com/Lionbull" target="_blank">https://github.com/Lionbull</a>',
-        "whotohire": "Are you serious?\nGeorgy Pokazeev of course!"
+        "whotohire": "Are you serious?\nGeorgy Pokazeev of course!",
     };
 
     const handleCommand = (command: string) => {
+        /**
+         * Generates the output for the command
+        */
         if (commands.hasOwnProperty(command)) {
             output += `$ ${command}\n${commands[command]}\n\n`;
         } else {
@@ -26,7 +30,21 @@
         }
     };
 
+    const generateHelpCommand = () => {
+        /**
+         * Function for a dymamic composition of the help command output
+         * Simplifies the process of adding new commands to the terminal
+         */
+        let helpCommandOutput = commands["help"];
+        helpCommandOutput = Object.keys(commands).filter(key => key !== "help").join(", ") + ", help";
+        commands["help"] = helpCommandOutput;
+    };
+
     const executeCommand = () => {
+        /**
+         * Function to execute the command
+         * Pushes the command to the history and resets the input
+        */
         handleCommand(input);
         commandHistory.push(input);
         historyIndex = commandHistory.length;
@@ -36,6 +54,10 @@
     };
 
     const handleKeyDown = async (event: KeyboardEvent) => {
+        /**
+         * Function to keyboard manipulation of the terminal
+         * @param event: KeyboardEvent
+        */
         if (event.key === "Enter") {
             executeCommand();
             scrollToBottom();
@@ -56,6 +78,9 @@
     };
 
     const scrollToBottom = async () => {
+        /**
+         * Function to scroll the terminal to the bottom when the output is too long 
+        */
         await tick();
         const terminalElement = document.getElementById("terminal");
         setTimeout(() => {
@@ -65,6 +90,9 @@
     };
 
     const focusInput = () => {
+        /**
+         * Function to focus the input when the terminal is clicked
+        */
         let selection = window.getSelection();
         if (!selection || selection.toString().length === 0) {
             document.getElementById("input")?.focus();
@@ -73,11 +101,13 @@
 
     onMount(async() => {
         await tick();
-        console.log(outputElement)
+        generateHelpCommand();
         focusInput();
     });
 </script>
 
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
 <div id="terminal" on:click={focusInput}>
     <div class="introduction-text">
         =======================================<br>
@@ -90,6 +120,7 @@
     <div id="output" bind:this={outputElement}>{@html output}</div>
     <div id="input-line">
         <span id="prompt">$&nbsp;</span>
+        <!-- svelte-ignore a11y-autofocus -->
         <input type="text" id="input" bind:value={input} on:keydown={handleKeyDown} autofocus>
     </div>
 </div>
