ab684852672cfcb4a5a22ba6adea19a4cad19616
GeorgyPk <georgy.pokazeev@tak.fi>
2024-07-01 02:26:19 +0300

    Final classic cv styles and layout

--- src/routes/classic_cv/+page.svelte
+++ src/routes/classic_cv/+page.svelte
@@ -1,122 +1,117 @@
 <script>
   import { onMount } from "svelte";
 
-  let name = "Georgy Pokazeev";
-  let title = "Software Engineer";
   let contactInfo = {
     phone: "+358456043575",
     email: "geo1529@yahoo.com",
     address: "Humalniementie 5 C 24, 00840 Helsinki, Finland",
   };
 
-  let experiences = [
-    {
-      role: "Full Stack Developer",
-      company: "Visitory Oy",
-      duration: "Jan 2022 - Present",
-      details:
-        "Main product provides statistics mainly on social media and tourism. Worked with data processing, data visualisation, web development, design, large SQL pipelines (part of data processing), databases, ",
-    },
-    {
-      role: "Junior Developer",
-      company: "Web Solutions",
-      duration: "Jun 2018 - Dec 2019",
-      details:
-        "Worked on web development projects and maintained existing applications.",
-    },
-  ];
-
-  let education = [
-    {
-      degree: "B.Sc. in Computer Science",
-      institution: "Anytown University",
-      duration: "2014 - 2018",
-    },
-  ];
-
-  let skills = ["JavaScript", "Svelte", "HTML", "CSS", "Node.js"];
+  let skills = ["Python", "Svelte", "SQL", "JavaScript", "CSS", "Databricks", "Django"];
 
   onMount(() => {});
 </script>
 
-<div class="cv-container">
-  <header class="header">
-    <div class="header-info-section">
-      <h1 id="name-title">{name}</h1>
-      <h2>{title}</h2>
-      <p><i class="fas fa-phone-alt"></i> {contactInfo.phone}</p>
-      <p><i class="fas fa-envelope"></i> {contactInfo.email}</p>
-      <p><i class="fas fa-map-marker-alt"></i> {contactInfo.address}</p>
-    </div>
-    <div class="photo-container">
-      <img
-        src="headshot_georgy.jpg"
-        alt="Georgy Pokazeev"
-        class="profile-photo"
-      />
-    </div>
-  </header>
+<div class="global-wrapper">
 
-  <section class="career-overview">
-    <h2>Career Overview</h2>
-    <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur
-      expedita voluptate nulla magnam dolorum eos nihil quo, fugiat libero in,
-      deleniti nobis perspiciatis nemo numquam similique! Sequi quis suscipit
-      officiis.
-    </p>
-  </section>
+    <button>Download as PDF</button>
 
-  <body>
-    <div class="left-content">
-        <h2>Education</h2>
-        <p>
-            M.Sc. (Software Engineering)
-        </p>
-        <p>
-            LUT University of Technology | 2023 - 2025
-        </p>
-        <p>
-            B.Sc. (Software Engineering)
-        </p>
-        <p>
-            LUT University of Technology | 2020 - 2023
-        </p>
-        <h2>Skills</h2>
-        <ul>
-            {#each skills as skill}
-              <li><i class="fas fa-check"></i> {skill}</li>
-            {/each}
-          </ul>
-        <h2>Reference</h2>
-        <p>Antti Honkanen</p>
-        <p>CEO | Visitory Oy</p>
-        <p>antti.honkanen@visitory.io</p>
-        <p>+3581234567</p>
-    </div>
-    <div class="right-content">
-        <h2>Experience</h2>
-        <h4>Full Stack Developer</h4>
-        <p>Visitory Oy | 2022 - Present</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat expedita fuga animi ipsa delectus, sapiente at aliquid debitis pariatur adipisci est voluptas molestias veniam, cumque sequi voluptatem nobis nihil maxime!</p>
+    <div class="cv-container">
+    <header class="header">
+        <div class="header-info-section">
+        <h1 id="name-title">Georgy Pokazeev</h1>
+        <h2>SOFTWARE ENGINEER</h2>
+        <p><i class="fas fa-phone-alt"></i> {contactInfo.phone}</p>
+        <p><i class="fas fa-envelope"></i> {contactInfo.email}</p>
+        <p><i class="fas fa-map-marker-alt"></i> {contactInfo.address}</p>
+        </div>
+        <div class="photo-container">
+        <img
+            src="headshot_georgy.jpg"
+            alt="Georgy Pokazeev"
+            class="profile-photo"
+        />
+        </div>
+    </header>
 
-        <h4>Full Stack Developer</h4>
-        <p>ByteTyde Oy | 2024 - Present</p>
-        <p>Self-employed<br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat expedita fuga animi ipsa delectus, sapiente at aliquid debitis pariatur adipisci est voluptas molestias veniam, cumque sequi voluptatem nobis nihil maxime!</p>
-        
-        <h4>Student Assistant</h4>
-        <p>LUT University | 2021 - 2022</p>
-        <p>Self-employed<br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat expedita fuga animi ipsa delectus, sapiente at aliquid debitis pariatur adipisci est voluptas molestias veniam, cumque sequi voluptatem nobis nihil maxime!</p>
+    <section class="career-overview">
+        <h2>CAREER OVERVIEW</h2>
+        <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur
+        expedita voluptate nulla magnam dolorum eos nihil quo, fugiat libero in,
+        deleniti nobis perspiciatis nemo numquam similique! Sequi quis suscipit
+        officiis.
+        </p>
+    </section>
+
+    <body>
+        <div class="left-content">
+            <div class="subsection">
+                <h2>EDUCATION</h2>
+                <div class="tight-text">
+                    <p>
+                        M.Sc. (Software Engineering)
+                    </p>
+                    <p>
+                        LUT University of Technology | 2023 - 2025
+                    </p>
+                </div>
+                <div class="tight-text">
+                    <p>
+                        B.Sc. (Software Engineering)
+                    </p>
+                    <p>
+                        LUT University of Technology | 2020 - 2023
+                    </p>
+                </div>
+            </div>
+            <div class="subsection">
+                <h2>SKILLS</h2>
+                <ul>
+                    {#each skills as skill}
+                    <li><i class="fas fa-check"></i> {skill}</li>
+                    {/each}
+                </ul>
+            </div>
+            <div class="subsection">
+                <h2>REFERENCE</h2>
+                <div class="tight-text">
+                    <p>Antti Honkanen</p>
+                    <p>CEO | Visitory Oy</p>
+                    <p>antti.honkanen@visitory.io</p>
+                    <p>+3581234567</p>
+                </div>
+            </div>
+        </div>
+        <div class="right-content">
+            <h2>EXPERIENCE</h2>
+            <h3>Full Stack Developer</h3>
+            <p>Visitory Oy | 2022 - Present</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat expedita fuga animi ipsa delectus, sapiente at aliquid debitis pariatur adipisci est voluptas molestias veniam, cumque sequi voluptatem nobis nihil maxime!</p>
+
+            <h3>Full Stack Developer</h3>
+            <p>ByteTyde Oy | 2024 - Present</p>
+            <p>Self-employed<br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat expedita fuga animi ipsa delectus, sapiente at aliquid debitis pariatur adipisci est voluptas molestias veniam, cumque sequi voluptatem nobis nihil maxime!</p>
+            
+            <h3>Study Assistant</h3>
+            <p>LUT University | 2021 - 2022</p>
+            <p>Self-employed<br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat expedita fuga animi ipsa delectus, sapiente at aliquid debitis pariatur adipisci est voluptas molestias veniam, cumque sequi voluptatem nobis nihil maxime!</p>
+        </div>
+    </body>
     </div>
-  </body>
 </div>
 
-<button>Download as PDF</button>
-
-<style>
+<style lang="scss">
   @import url("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css");
   @import url("https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap");
 
+    .global-wrapper {
+        display: flex;
+        width: 100vw;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
   .header-info-section {
     font-family: "Montserrat";
     flex: 3;
@@ -185,13 +180,18 @@
     object-fit: cover;
   }
 
+  .subsection {
+    margin-bottom: 35px;
+  }
+
   ul {
     list-style: none;
     padding: 0;
   }
 
   ul li {
-    margin: 5px 0;
+    margin: 10px 0;
+    font-size: 1.2rem;
     color: #333;
   }
 
@@ -213,5 +213,35 @@
 
   button:hover {
     background-color: #45a049;
+  }
+
+  h2 {
+    padding-bottom: 0;
+  }
+
+  h3 {
+    padding-bottom: 0px;
+    margin: 0;
+    font-size: 1.3rem;
+    margin-top: 30px;
+    text-decoration: underline;
+    text-decoration-thickness: 2px;
+  }
+
+  p {
+    font-size: 1.2rem;
+  }
+
+  .tight-text {
+    margin-bottom: 20px;
+
+    & p {
+      margin: 10px 0;
+    }
+
+    & h2 {
+        margin: 10px 0;
+    }
+
   }
 </style>
