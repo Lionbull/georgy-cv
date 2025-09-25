62fe4757b989249477d87bcad606f6f9d48b33ed
GeorgyPk <georgy.pokazeev@tak.fi>
2025-09-25 08:55:44 +0300

    Add SEO

---
+++ src/routes/classic_cv/+page.js
@@ -0,0 +1,12 @@
+export const prerender = true;
+
+export async function load() {
+    return {
+        meta: {
+            title: 'Georgy Pokazeev - Classic CV | Software Engineer Resume',
+            description: 'Download my professional CV. Full Stack Developer with 4+ years experience at Rantalainen Oy and Visitory Oy. Master\'s student at LUT University.',
+            keywords: 'Georgy Pokazeev CV, Software Engineer Resume, Full Stack Developer, Python, Django, Angular, Svelte, TypeScript, LUT University, Helsinki, Finland',
+            canonicalUrl: 'https://georgypokazeev.com/classic_cv'
+        }
+    };
+}
